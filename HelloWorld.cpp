c2cada9d4f0fff2c4587b8cdcb55e4165fef74d2
Malik Salem <malik.salem@mths.ca>
2022-09-16 15:21:36 +0000

    code

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 maliksalem1 All rights reserved
+//
+// Created by: maliksalem1
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.\n";
+}
